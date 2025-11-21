0a8cddcb00524d3c7d1d8ba60fb668bf59fcf9a2
b25albjo <b25albjo@student.his.se>
2025-11-21 15:53:34 +0100

    Added Playing Random Sound from list
    Random sound play when you pick up coin

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Random = System.Random;
 
 [RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
@@ -11,8 +12,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 3;
 
     [SerializeField] TextMeshProUGUI coinText;
+
+    [SerializeField] private AudioClip[] coinPickUpSounds;
     
     private Rigidbody2D myRigidbody2D;
+
+    private AudioSource myAudioSource;
     
     private InputAction move;
     
@@ -20,6 +25,8 @@ public class PlayerMovement : MonoBehaviour
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myRigidbody2D.gravityScale = 0;
+
+        myAudioSource = GetComponent<AudioSource>();
         
         move = InputSystem.actions.FindAction("Move");
     }
@@ -43,6 +50,8 @@ public class PlayerMovement : MonoBehaviour
         if (other.CompareTag("Coin"))
         {
             coins++;
+            myAudioSource.clip = coinPickUpSounds[new Random().Next(0, coinPickUpSounds.Length - 1)];
+            myAudioSource.Play();
             coinText.text = $"Coins:{coins}";
             Destroy(other.gameObject);
         }
